df39f9c28ba36fe0d73d201c349318d3e9580a96
Cefer Mehtiyev <cefer20051965@gmail.com>
2024-07-05 22:56:52 +0400

    first

---
+++ src/main/java/com/example/demoingress/DemoingressApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoingress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoingressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoingressApplication.class, args);
+	}
+
+}
